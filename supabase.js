b9cb4fed2e400790ae971dd4e5474c0093a1e5fb
apexmarke <moohccaptain@gmail.com>
2026-08-08 10:35:06 +0300

    Update Supabase publishable key

--- supabase.js
+++ supabase.js
@@ -1,7 +1,7 @@
 const SUPABASE_URL = "https://bkhqbzegquykosrpbfnt.supabase.co";
 
 const SUPABASE_PUBLISHABLE_KEY =
-    "sb_publishable_";
+    sb_publishable_-4345PeuHRh0Cxw4OJVQrA_FPaSZF4q
 
 const supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
