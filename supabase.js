ab5712f46b7396041b7ba18fbba84fa743886abd
apexmarke <moohccaptain@gmail.com>
2026-08-08 11:47:33 +0300

    Format supabase.js for better readability

--- supabase.js
+++ supabase.js
@@ -1,9 +1,11 @@
-const SUPABASE_URL = "https://bkhqbzegquykosrpbfnt.supabase.co";
+const SUPABASE_URL =
+    "https://bkhqbzegquykosrpbfnt.supabase.co";
 
 const SUPABASE_PUBLISHABLE_KEY =
-    sb_publishable_-4345PeuHRh0Cxw4OJVQrA_FPaSZF4q
+    "sb_publishable_-4345PeuHRh0Cxw4OJVQrA_FPaSZF4q";
 
-const supabaseClient = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_PUBLISHABLE_KEY
-);
+const supabaseClient =
+    window.supabase.createClient(
+        SUPABASE_URL,
+        SUPABASE_PUBLISHABLE_KEY
+    );
